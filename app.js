4703fa01d2af3c82f3dc52c8e58f4b40b052d750
root <root@DESKTOP-6B3FQL8.localdomain>
2022-07-12 14:26:43 -0500

    had to restart but looks more clean now

--- app.js
+++ app.js
@@ -1,84 +1,149 @@
-const date = new Date();
+let nav = 0;
+let clicked = null;
+// make and array for the reminder events
+let events = [];
 
-// make the calendar
-const renderCalendar = () => {
-  date.setDate(1);
+// get the calendar id
+const calendar = document.getElementById('calendar');
+// get the newEventTitle id
+const newEventTitle = document.getElementById('newEventTitle');
+// get the deleteEventTitle id
+const deleteEventTitle = document.getElementById('deleteEventTitle');
+// get the modalBackDrop id
+const backDrop = document.getElementById('modalBackDrop');
+// get the eventTitleInput id
+const eventTitleInput = document.getElementById('eventTitleInput');
 
-//   get the days id from html
-  const monthDays = document.getElementById("days");
+// Make an Array for the week days
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  const lastDay = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
+// make a function that allows the days to be clicked to add reminders
+openModal = (date) => {
+  clicked = date;
 
-  const prevLastDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    0
-  ).getDate();
+  const eventForDay = events.find(e => e.date === clicked);
 
-  const firstDayIndex = date.getDay();
-
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDay();
-
-  const nextDays = 7 - lastDayIndex - 1;
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-  let days = "";
-
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  if (eventForDay) {
+    document.getElementById('eventText').innerText = eventForDay.title;
+    deleteEventTitle.style.display = 'block';
+  } else {
+    newEventTitle.style.display = 'block';
   }
 
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
-    } else {
-      days += `<div>${i}</div>`;
+  backDrop.style.display = 'block';
+}
+
+load = () => {
+  const dt = new Date();
+
+  if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
+  }
+
+  // let day equal current day
+  const day = dt.getDate();
+  // let month equal current month
+  const month = dt.getMonth();
+  // let year equal current year
+  const year = dt.getFullYear();
+
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+  document.getElementById('monthDisplay').innerText =
+    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
+  calendar.innerHTML = '';
+
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement('div');
+    daySquare.classList.add('day');
+
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+    if (i > paddingDays) {
+      daySquare.innerText = i - paddingDays;
+      const eventForDay = events.find(e => e.date === dayString);
+
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.id = 'currentDay';
+      }
+
+      if (eventForDay) {
+        // make a new div
+        const eventDiv = document.createElement('div');
+        // add event to that div
+        eventDiv.classList.add('event');
+        eventDiv.innerText = eventForDay.title;
+        // attach the eventDiv to the daySquare div id
+        daySquare.appendChild(eventDiv);
+      }
+
+      daySquare.addEventListener('click', () => openModal(dayString));
+    } else { // leave the days that not apart of that month blank
+      daySquare.classList.add('padding');
     }
+    // attach the days to the calendar
+    calendar.appendChild(daySquare);
   }
+}
 
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
+closeModal = () => {
+  newEventTitle.style.display = 'none';
+  deleteEventTitle.style.display = 'none';
+  backDrop.style.display = 'none';
+  eventTitleInput.value = '';
+  clicked = null;
+  load();
+}
+
+// make a way to add reminders
+saveEvent = () => {
+  if (eventTitleInput.value) {
+    // adds the reminder to the calendar
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+    });
+    // when save is click adds to calendar
+    closeModal();
   }
-};
+}
+// make a way to delete the event reminder
+deleteEvent = () => {
+  events = events.filter(e => e.date !== clicked);
+  closeModal();
+}
 
-document.querySelector(".prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
-});
+initButtons = () => {
+  // get the left arrow from html and add event listener to it when clicked to move back a month
+  document.querySelector('.next').addEventListener('click', () => {
+    nav++;
+    load();
+  });
+  // get the right arrow from html and add event listener to it when clicked to move to the next month
+  document.querySelector('.prev').addEventListener('click', () => {
+    nav--;
+    load();
+  });
 
-document.querySelector(".next").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar();
-});
-
-renderCalendar();
+  // get save button from html and add event listener to it when clicked
+  document.getElementById('saveButton').addEventListener('click', saveEvent);
+  //  get cancel button from html and add event listener to it when clicked
+  document.getElementById('cancelButton').addEventListener('click', closeModal);
+  //  get delete button from html and add event listener to it when clicked
+  document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+  //  get close button from html and add event listener to it when clicked
+  document.getElementById('closeButton').addEventListener('click', closeModal);
+}
+// call the function
+initButtons();
+load();
